131646da7d5e1e9d37e697fa6aeb9ac56723a197
ChrisA95G <christopher@arleth.dev>
2026-09-03 20:23:22 +0200

    refactor(adb): remove remote cheat management

--- gui/src-tauri/src/lib.rs
+++ gui/src-tauri/src/lib.rs
@@ -100,9 +100,6 @@ pub fn run() {
             rom_cache::set_rom_path_manual,
             rom_cache::scan_and_update_rom_cache,
             // cheats
-            cheats::install_cheat_android,
-            cheats::list_installed_cheats_android,
-            cheats::delete_cheat_android,
             cheats::install_cheat_pc,
             cheats::list_installed_cheats_pc,
             cheats::delete_cheat_pc,
