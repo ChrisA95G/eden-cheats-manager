9571f8bfe7e700613b7162708ed1a9420903ed80
ChrisA95G <christopher@arleth.dev>
2026-09-05 08:20:31 +0200

    feat(ui): add Material 3 primitives

--- gui/src/routes/+layout.js
+++ gui/src/routes/+layout.js
@@ -1,5 +1,6 @@
 import '$lib/styles/tokens.css';
 import '$lib/styles/base.css';
+import '$lib/styles/components.css';
 
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we use adapter-static with a fallback to index.html to put the site in SPA mode
